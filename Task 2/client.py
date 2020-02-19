de7cf864ea033dcbc209073c5977d77383461058
Alguien de la UTP <alguien@utp.edu.co>
2020-02-19 15:47:41 -0500

    client send chunks

--- Task 2/client.py
+++ Task 2/client.py
@@ -15,16 +15,31 @@ print("Connecting to the server...")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
+# Chunk's Size: 2MB
+ps = 2 * 1024 * 1024
+
+part = 0
 filepath = sys.argv[1]
 file = open(filepath, 'rb')
-content = file.read()
-file.close();
 
-hashing = hashlib.md5(content).digest()
+while True:
 
-# Enviamos el nombre del archivo, el contenido y un hashing para demostrar integridad
-socket.send_multipart([filepath.encode(), content, hashing])
+	part = part + 1
+	chunk = file.read(ps)
 
-# Recibimos y mostramos el resultado
-result = socket.recv()
-print("Resultado: %s" % result)
+	if not chunk:
+		break
+
+	hashing = hashlib.md5(chunk).digest()
+
+	filename = filepath # TODO
+	filename = filename + ".part_" + str(part)
+
+	# Enviamos el nombre del archivo, el contenido y un hashing para demostrar integridad
+	socket.send_multipart([filename.encode(), chunk, hashing])
+
+	# Recibimos y mostramos el resultado
+	result = socket.recv()
+	print("Resultado: %s" % result)
+
+file.close()
